141d597fd9ecb8cdbfd93f9f381b557863c6dfa1
alice920524 <2283868198@qq.com>
2016-05-02 20:50:14 +0800

    Update to Github for first time!

---
+++ src/com/atguigu/github/test/HelloGitForTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.github.test;
+
+public class HelloGitForTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Github for Test, First!");
+		
+	}
+
+}
