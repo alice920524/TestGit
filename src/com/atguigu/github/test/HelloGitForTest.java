b16be3effadca16127956d9209a74cd356d9a55e
alice920524 <2283868198@qq.com>
2016-05-03 01:36:43 +0800

    Update 02 for test!

--- src/com/atguigu/github/test/HelloGitForTest.java
+++ src/com/atguigu/github/test/HelloGitForTest.java
@@ -5,6 +5,7 @@ public class HelloGitForTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Github for Test, First!");
+		System.out.println("Hello Github for Test, Second!");
 		
 	}
 
